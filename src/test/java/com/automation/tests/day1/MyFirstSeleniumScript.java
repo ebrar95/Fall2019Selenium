ada4bb1f9622cd0ae1ffa8bc5e0cdd0aa4d5239e
nigarebrarokc@gmail.com <nigarebrarokc@gmail.com>
2020-03-01 11:41:31 -0600

    first commit

---
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -0,0 +1,14 @@
+package com.automation.tests.day1;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class MyFirstSeleniumScript {
+    public static void main(String[] args) {
+        // setup chrome driver
+        WebDriverManager.chromedriver().setup();
+        // create chrome driver object
+        ChromeDriver driver = new ChromeDriver();
+        //open some website
+        driver.get("http://google.com");
+    }
+}
